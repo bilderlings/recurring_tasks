8873d17e8e386688b48c0d606f9bd14605b867cb
Dmitry Migunov <d.migunovs@bilderlings.com>
2025-01-08 11:10:21 +0200

    Clonner fix

--- lib/cloner.rb
+++ lib/cloner.rb
@@ -1,75 +1,81 @@
+require 'logger'
+
 class Cloner
+  @logger = Logger.new($stdout)
+  @logger.level = Logger::INFO
+
+  def self.logger
+    @logger
+  end
+
   def self.clone(issue_id, duration, recurring_field_id)
-      original = Issue.find_by_id(issue_id)
-      return unless original
+    original = Issue.find_by_id(issue_id)
+    unless original
+      logger.warn("Original issue not found Issue##{issue_id}")
+      return
+    end
 
-      existing = IssueRelation.
-        where(issue_from: original.id, relation_type: IssueRelation::TYPE_COPIED_TO).
-        order(issue_to_id: :desc).
-        limit(1).
-        first
+    existing = IssueRelation.
+      where(issue_from: original.id, relation_type: IssueRelation::TYPE_COPIED_TO).
+      order(issue_to_id: :desc).
+      limit(1).
+      first
 
-      if existing
-        existing_issue = Issue.find_by_id(existing.issue_to)
-        return unless existing_issue
-        return unless existing_issue.start_date <= duration.ago
-      else
-        return unless original.start_date <= duration.ago
+    if existing
+      existing_issue = Issue.find_by_id(existing.issue_to)
+      unless existing_issue
+        logger.warn("Existing issue not found Issue##{existing.issue_to}")
+        return
       end
 
-      copy = self.clone_issue_with_children(original)
-      copy.start_date = Time.now
-
-      if original.due_date.present?
-        issue_date = (original.start_date || original.created_on).to_date
-        copy.due_date = copy.start_date + (original.due_date - issue_date)
+      unless existing_issue.start_date <= duration.ago
+        logger.info("Nothing to do for now: latest start_date = #{existing_issue.start_date} for Issue##{existing_issue.id}")
+        return
       end
-
-      copy.children.each do |child|
-        child.start_date = Time.now
-
-        if original.due_date.present?
-          issue_date = (original.start_date || original.created_on).to_date
-          child.due_date = child.start_date + (original.due_date - issue_date)
-        end
-
-        child.reload
-        child.save!
+    else
+      unless original.start_date <= duration.ago
+        logger.info("Nothing to do for now: latest start_date = #{original.start_date} for Issue##{original.id}")
+        return
       end
+    end
 
-      copy.custom_field_values = copy.custom_field_values.inject({}) do |h, v|
+    due_date = if original.due_date.present?
+      issue_date = (original.start_date || original.created_on).to_date
+      Date.today + (original.due_date - issue_date)
+    end
+
+
+    ActiveRecord::Base.transaction do
+      copied_issue =Issue.new.copy_from(original, attachments: false)
+      copied_issue.custom_field_values = copied_issue.custom_field_values.inject({}) do |h, v|
         h[v.custom_field_id] = v.custom_field_id == recurring_field_id ? nil : v.value
         h
       end
+      copied_issue.save!
+      copied_issue.reload
+      copied_issue.start_date = Date.today
+      copied_issue.due_date = due_date
+      copied_issue.save!
+      copied_issue.reload
 
-      copy.reload
-      copy.save!
-      puts("Issue##{original.start_date} cloned to ##{copy.id}")
-  end
-
-  def self.clone_issue_with_children(original_issue)
-      cloned_issue = Issue.new.copy_from(original_issue, { attachments: false, watchers: true })
-      cloned_issue.parent_issue_id = original_issue.parent_issue_id
-      cloned_issue.save!
-
-      original_issue.children.each do |child_issue|
-          cloned_child_issue = clone_issue_with_children(child_issue)
-          cloned_child_issue.parent_issue_id = cloned_issue.id
-          cloned_child_issue.reload
-          cloned_child_issue.save!
+      copied_issue.children.each do |child_issue|
+        child_issue.start_date = Date.today
+        child_issue.due_date = due_date
+        child_issue.save!
       end
 
-      cloned_issue
+      logger.info("Issue:#{issue_id} copied to Issue:#{copied_issue.id}")
+    end
   end
 
   def self.duration(variant)
-      case variant
-      when 'Weekly' then 1.week
-      when 'Monthly' then 1.month
-      when 'Quarterly' then 3.months
-      when 'Semiannually' then 6.months
-      when 'Annually' then 1.year
-      else nil
-      end
+    case variant
+    when 'Weekly' then 1.week
+    when 'Monthly' then 1.month
+    when 'Quarterly' then 3.months
+    when 'Semiannually' then 6.months
+    when 'Annually' then 1.year
+    else nil
+    end
   end
 end
